9ddb3f6234f2aca4a44c841edf8134cb85fc3d8e
Jackson <jackson.smith@sanar.com>
2023-05-23 17:15:04 -0300

    refactor: improve model list

--- final_project/View/MoviesList/MoviesListController.swift
+++ final_project/View/MoviesList/MoviesListController.swift
@@ -44,26 +44,31 @@ class MoviesListController: UIViewController, Coordinating, UITableViewDataSourc
         viewModel.getMovies()
     }
     
-    func bindSetup() {
+    // MARK: - Private Functions
+    
+    private func bindSetup() {
         viewModel = MoviesViewModel(movies: MoviesModel(), service: MoviesService())
         viewModel.reloadTable = self.reloadTable
     }
     
-    func setupContraints() {
+    private func setupContraints() {
         tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
     }
     
-    func didTapCell(position: IndexPath) {
+    private func didTapCell(position: IndexPath) {
         let id = String(data[position.row].id)
         
-        // BEFORE COORDINATOR
-        //        self.present(MovieDetailsController(movieId: id), animated: true)
+        // WITHOUT COORDINATOR
+        // self.present(MovieDetailsController(movieId: id), animated: true)
         coordinator?.navigate(to: .moviesDetails, data: id)
     }
     
+    // MARK: - Public Functions
+    
+    // BIND VIEW - REACTIVE RELOAD FROM VIEW MODEL
     func reloadTable() {
         DispatchQueue.main.async {
             self.tableView.reloadData()
